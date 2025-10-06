5ce985fc46fd446e1b6a5c051030294b50ef94ac
JasperDavidson <jasperdavdsn@gmail.com>
2025-10-05 22:47:02 -0400

    GPU Executor abstract class created

---
+++ inc/IGPUExector.h
@@ -0,0 +1,38 @@
+#ifndef IGPU_H
+#define IGPU_H
+
+#include <span>
+
+// TODO: Add more options as interface is built out
+enum class GPUState { GPUSuccess, GPUFailure };
+
+class GPUBufferHandle {
+public:
+  int ID;
+};
+
+class IGPUExecutor {
+public:
+  // Allocating/freeing buffer memory on the GPU for kernel tasks
+  GPUBufferHandle virtual allocate_buffer(std::uint32_t buffer_size) = 0;
+  GPUState virtual deallocate_buffer(GPUBufferHandle buffer_handle) = 0;
+
+  // Sending memory between devices for task completion/after task completion
+  GPUState virtual copy_to_device(std::span<const std::byte>,
+                                  GPUBufferHandle buffer_handle,
+                                  std::uint32_t data_size) = 0;
+  GPUState virtual copy_from_device(std::span<std::byte>,
+                                    GPUBufferHandle buffer_handle,
+                                    std::uint32_t data_size) = 0;
+
+  // Maybe a string isn't the best hash?
+  GPUState virtual execute_kernel(const std::string &kernel_name) = 0;
+
+  // Prevents more GPU tasks from being added until all current ones are
+  // complete
+  GPUState virtual synchronize() = 0;
+
+  virtual ~IGPUExecutor() = default;
+};
+
+#endif
