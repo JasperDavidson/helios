034340e678cda1db29fe975df0454902e4235d12
JasperDavidson <jasperdavdsn@gmail.com>
2025-10-27 18:23:38 -0400

    Created task graph structure, and implemented adding tasks

--- inc/Tasks.h
+++ inc/Tasks.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <tuple>
 #include <type_traits>
+#include <unordered_map>
 #include <vector>
 
 // Forward declaration of Scheduler class
@@ -75,9 +76,41 @@ class GPUTask : public ITask {
     void accept(const Scheduler &scheduler) override;
 };
 
+/*
+ * TaskGraph
+ * Represent tasks (nodes) and their dependencies (edges)
+ *
+ *  So what should the task graph actually contain?
+ *  Instance variables:
+ *      - all_tasks_: map<int, shared_ptr<ITask>>
+ *      - dependencies_: map<int (taskID), vector<int> (taskIDs)> - maps a task to all tasks that depend on it
+ *      - dependent_: map<int, (taskID), vector<int> (taskIDs)> - maps a task to all tasks that it depends on
+ *      - data_producer_map_: map<int (dataID), int (taskID)> - maps a data handle to the task that produces it
+ *      - unfulfilled_data_: map<int (dataID), vector<int> (taskIDs)> - maps a data handle with no producer to a vector
+ * tasks that require it
+ * Methods:
+ *      - add_task(ITask& task): adds a task to the graph following this algorithm
+ *          1. Add a shared pointer to the task to the all_tasks_ map
+ *          2. Iterate over the output data handles and add data_producer_map_[handle->ID] = new_task->ID
+ *              - What if a task already claimed to produce this data? (data_producer_map_.find(handle->ID) != .end())
+ *              - Throw a runtime exception, since only one task con produce a certain piece of data
+ *          3. Iterate over the input data handles and fetch their producer_task_ids.
+ *              - If data_producer_map_[handle->ID] doesn't exist then add new_task->ID to unfulfilled_data_[handle->ID]
+ *              - Add producer_task_id to list at dependencies_map_[new_task->ID]
+ *              - Add new_task->ID to list at dependents_map_[producer_task_id]
+ *              - If handle->ID is a key in unfulfilled_data_ iterate over the tasks and do
+ * depedencies[unfilfilled_task->ID] = new_task->ID
+ */
 class TaskGraph {
+  public:
+    void add_task(std::shared_ptr<ITask> task);
+
   private:
-    // Adjacency list structure --> Graph should not be dense so this saves on memory
+    std::unordered_map<int, std::shared_ptr<ITask>> all_tasks_;
+    std::unordered_map<int, std::vector<int>> dependencies_;
+    std::unordered_map<int, std::vector<int>> dependents_;
+    std::unordered_map<int, int> data_producer_map_;
+    std::unordered_map<int, std::vector<int>> unfulfilled_data_;
 };
 
 #endif
--- src/Tasks.cpp
+++ src/Tasks.cpp
@@ -1,8 +1,45 @@
 #include "Tasks.h"
 #include "Scheduler.h"
+#include <stdexcept>
 
 template <typename F, class... Types> void CPUTask<F, Types...>::accept(const Scheduler &scheduler) {
     scheduler.visit(*this);
 }
 
 void GPUTask::accept(const Scheduler &scheduler) { scheduler.visit(*this); }
+
+void TaskGraph::add_task(std::shared_ptr<ITask> task) {
+    all_tasks_[task->ID] = task;
+
+    // Mapping each output of the task to the task itself
+    for (auto &output_handle : task->outputs) {
+        if (data_producer_map_.find(output_handle->ID) != data_producer_map_.end()) {
+            throw std::runtime_error(
+                "Error during TaskGraph construction: Attempted to assign multiple tasks to one data output");
+        }
+
+        data_producer_map_[output_handle->ID] = task->ID;
+    }
+
+    for (auto &input_handle : task->inputs) {
+        // Marks the data as unfulfilled if no task has currently promised to produce it
+        if (data_producer_map_.find(input_handle->ID) == data_producer_map_.end()) {
+            unfulfilled_data_[input_handle->ID].push_back(task->ID);
+            continue;
+        }
+
+        int data_producer_id = data_producer_map_[input_handle->ID];
+        dependencies_[task->ID].push_back(data_producer_id);
+        dependents_[data_producer_id].push_back(task->ID);
+
+        // If the data was previously unfilled, mark it as filled and update the tasks that required it
+        if (unfulfilled_data_.find(input_handle->ID) != unfulfilled_data_.end()) {
+            for (int unfulfilled_task_id : unfulfilled_data_[input_handle->ID]) {
+                dependencies_[unfulfilled_task_id].push_back(task->ID);
+                dependents_[task->ID].push_back(unfulfilled_task_id);
+            }
+
+            unfulfilled_data_.erase(input_handle->ID);
+        }
+    }
+}
